ac4fea0579a3a20b4fbc30a7571415eb22d32264
Your Name <hautran1993@yahoo.com>
2018-01-20 16:03:05 -0600

    main:

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require("express-handlebars");
 
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3306;
 
 // Requiring our models for syncing
 var db = require("./models");
