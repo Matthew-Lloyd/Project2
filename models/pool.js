f1144c8c33d8a7c8ed93f2bb31706e1ef7ef89d6
Matthew-Lloyd <MLloyd09@gmail.com>
2018-01-24 21:40:00 -0600

    Got the sequelize database object fine-tuned. Deleted all the excess code

--- models/pool.js
+++ models/pool.js
@@ -1,36 +1,36 @@
 module.exports = function (sequelize, DataTypes) {
     var Pool = sequelize.define("Pool", {
         hashRate: {
-            type: DataTypes.DECIMAL(20, 20),
+            type: DataTypes.DECIMAL(16, 2),
             allowNull: false,
         },
         miners: {
-            type: DataTypes.DECIMAL,
+            type: DataTypes.DECIMAL(16, 2),
             allowNull: false,
         },
         workers: {
-            type: DataTypes.DECIMAL,
+            type: DataTypes.DECIMAL(16, 2),
             allowNull: false,
         },
         blocksPerHour: {
-            type: DataTypes.DECIMAL,
+            type: DataTypes.DECIMAL(16, 2),
             allowNull: false,
         },
-        priceUSD: {
-            type: DataTypes.STRING,
-            // allowNull: false,
+        usd: {
+            type: DataTypes.DECIMAL(16, 2),
+            allowNull: false,
         },
-        priceBTC: {
-            type: DataTypes.STRING,
-            // allowNull: false,
+        btc: {
+            type: DataTypes.DECIMAL(16, 2),
+            allowNull: false,
         },
     });
-    Pool.associate = function (models) {
-        // Associating Pool with Posts
-        // When an Pool is deleted, also delete any associated Posts
-        Pool.hasMany(models.topMiners, {
-            onDelete: "cascade"
-        });
-    };
+    // Pool.associate = function (models) {
+    //     // Associating Pool with Posts
+    //     // When an Pool is deleted, also delete any associated Posts
+    //     Pool.hasMany(models.topMiners, {
+    //         onDelete: "cascade"
+    //     });
+    // };
     return Pool;
 };
