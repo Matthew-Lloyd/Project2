012143752c19777e1bc00e134aee611334fd51af
Your Name <hautran1993@yahoo.com>
2018-01-26 07:13:54 -0600

    minor tweaks

--- public/js/history.js
+++ public/js/history.js
@@ -15,80 +15,10 @@ $(document).ready(function() {
             $("#shares").append(response.data[143].validShares);
             $("#hash-rates").append(response.data[143].averageHashrate);
             $("#stale-shares").append(response.data[143].staleShares);
-            // console.log(response.data[143]); 
+            console.log(response.data[143]); 
             
           
         });//ending of first ajax calls
-        var miner = localStorage.getItem("minerId");  
-        var queryURL= "https://api.ethermine.org/miner/" + miner + "/history";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          })
-            .done(function (response) {
-              var xArray = [];
-              var yArray = [];
-              var yArray2 = [];
-              var yArray3 = [];
-              for (var i = 0; i < response.data.length; i++) {
        
-                // console.log(data);
-               
-                var time = new Date(response.data[i].time * 1000);
-               //  console.log(time);
-                yArray.push(response.data[i].currentHashrate / 1000000);
-                // xArray.push(time);
-                xArray.push(time);
-                yArray2.push(response.data[i].averageHashrate / 1000000);
-                yArray3.push(response.data[i].reportedHashrate / 1000000);
-               //  console.log(xArray);
-               //  console.log(yArray);
-               //  console.log(yArray2);
-               //  console.log(yArray3);
-                // var time2 = new Date(1516737600);
-                // time2.toString("MMM dd"); 
-                // console.log(time2);
-              };
-              var trace1 = {
-                x: xArray,
-                y: yArray,
-                name: 'current'
-              }
-       
-              var trace2 = {
-                x: xArray,
-                y: yArray2,
-                name: 'average'
-              }
-       
-              var trace3 = {
-                x: xArray,
-                y: yArray3,
-                name: 'recorded'
-              }
-       
-              var data = [
-                trace1, 
-                trace2, 
-                trace3,
-             ];
-       
-              var layout = {
-                title: 'Line plot of hashrates',
-                paper_bgcolor: 'rgb(255, 223, 0)',
-                plot_bgcolor: 'rgb(255, 255, 255)',
-                xaxis: {
-                  title: 'Date'
-                },
-                yaxis: {
-                  title: 'Hashrate'
-                }
-              };
-             
-              // console.log(xArray);
-           //    console.log(response.data);
-              //  console.log(time);
-              Plotly.newPlot('graph', data, layout);
-            });
     };
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,15 +11,9 @@ module.exports = function(app) {
         title: "title"
       });
   });
-
-  app.get("/history", function (req, res) {
-    // var minerid = req.params.minerid;
-    res.render('history', {title:"history"});
-  });
-
   app.get("/history/:minerId", function (req, res) {
     const params = req.params.minerid;
-    return res.render('history',{
+    res.render('history',{
       title: "history"
     });
   });
@@ -36,4 +30,23 @@ module.exports = function(app) {
       title: "title"
     });    
   });
-};  
+  //getting route
+  // app.get("/history:minerid", function(req, res) {
+  //   var condition = "id = " + req.params.id;
+  
+  //   id.all(condition, function(result) {
+  //     if (result.affectedRows == 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     } else {
+  //       res.status(200).end();
+  //     }
+  //   });
+  // });
+
+  //ending of exports  
+
+  // If no matching route is found default to home
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  };
