18f073356b2bfffc5fbc729ef67e83a6297f6046
Matthew-Lloyd <MLloyd09@gmail.com>
2018-01-25 19:08:06 -0600

    restored the history page from being down

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,3 @@
-//html routes to home Page
-//has to create the correct path to link to the html file
 var path = require("path");
 //=======================================
 
@@ -23,13 +21,14 @@ module.exports = function(app) {
   //   });
   // });
 
-  // app.get("history/:minerId", function (req, res) {
-  //   var minerid = req.params.minerid;
-  //   res.render('history', {title:"history"});
-  // });
-  app.get("/history/7Fb21ac4Cd75d9De3E1c5D11D87bB904c01880fc", function (req, res) {
+  app.get("/history", function (req, res) {
     // var minerid = req.params.minerid;
-    res.render('history',{
+    res.render('history', {title:"history"});
+  });
+
+  app.get("/history/:minerId", function (req, res) {
+    const params = req.params.minerid;
+    return res.render('history',{
       title: "history"
     });
   });
